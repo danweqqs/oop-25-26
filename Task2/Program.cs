85f0137584afcc0199a8b8097c6b5e8efdb8bfce
danweqqs <daniilsorokin381@gmail.com>
2025-11-23 21:28:45 +0200

    hw7

---
+++ Task2/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Program
+    {
+        class Pizza
+        {
+            string dough;
+            string sauce;
+            string topping;
+            
+            public Pizza() { }
+            
+            public void SetDough(string d) { dough = d; }
+            public void SetSauce(string s) { sauce = s; }
+            public void SetTopping(string t) { topping = t; }
+            
+            public void Info()
+            {
+                Console.WriteLine("Dough: {0}\nSause: {1}\nTopping: {2}",
+                dough, sauce, topping);
+            }
+        }
+
+        //Abstract Builder
+        abstract class PizzaBuilder
+        {
+            protected Pizza pizza;
+            
+            public PizzaBuilder() { }
+            
+            public Pizza GetPizza() { return pizza; }
+            public void CreateNewPizza() { pizza = new Pizza(); }
+            
+            public abstract void BuildDough();
+            public abstract void BuildSauce();
+            public abstract void BuildTopping();
+        }
+
+        //Concrete Builder 1
+        class HawaiianPizzaBuilder : PizzaBuilder
+        {
+            public override void BuildDough() { pizza.SetDough("cross"); }
+            public override void BuildSauce() { pizza.SetSauce("mild"); }
+            public override void BuildTopping() {
+                pizza.SetTopping("ham+pineapple"); }
+        }
+
+        //Concrete Builder 2
+        class SpicyPizzaBuilder : PizzaBuilder
+        {
+            public override void BuildDough() { pizza.SetDough("pan baked"); }
+            public override void BuildSauce() { pizza.SetSauce("hot"); }
+            public override void BuildTopping() {
+                pizza.SetTopping("pepparoni+salami"); }
+        }
+
+        //новий клас Concrete Builder 3
+        class MargaritaPizzaBuilder : PizzaBuilder
+        {
+            public override void BuildDough() { pizza.SetDough("thin"); }
+            public override void BuildSauce() { pizza.SetSauce("tomato"); }
+            public override void BuildTopping() {
+                pizza.SetTopping("mozzarella+basil"); }
+        }
+
+        /** "Director" */
+        class Waiter
+        {
+            private PizzaBuilder pizzaBuilder;
+            
+            public void SetPizzaBuilder(PizzaBuilder pb) { pizzaBuilder = pb; }
+            public Pizza GetPizza() { return pizzaBuilder.GetPizza(); }
+            
+            public void ConstructPizza()
+            {
+                pizzaBuilder.CreateNewPizza();
+                pizzaBuilder.BuildDough();
+                pizzaBuilder.BuildSauce();
+                pizzaBuilder.BuildTopping();
+            }
+        }
+
+        /** A customer ordering a pizza. */
+        class BuilderExample
+        {
+            public static void Main(String[] args)
+            {
+                Waiter waiter = new Waiter();
+                
+                // Створення та замовлення Гавайської піци
+                PizzaBuilder hawaiianPizzaBuilder = new HawaiianPizzaBuilder();
+                waiter.SetPizzaBuilder(hawaiianPizzaBuilder);
+                waiter.ConstructPizza();
+                Pizza pizza = waiter.GetPizza();
+                Console.WriteLine("--- Hawaiian Pizza ---");
+                pizza.Info();
+
+                // Створення та замовлення Гострої піци
+                PizzaBuilder spicyPizzaBuilder = new SpicyPizzaBuilder();
+                waiter.SetPizzaBuilder(spicyPizzaBuilder);
+                waiter.ConstructPizza();
+                pizza = waiter.GetPizza();
+                Console.WriteLine("\n--- Spicy Pizza ---");
+                pizza.Info();
+
+                // Створення та замовлення піци Маргарита новий клас
+                PizzaBuilder margaritaPizzaBuilder = new MargaritaPizzaBuilder();
+                waiter.SetPizzaBuilder(margaritaPizzaBuilder);
+                waiter.ConstructPizza();
+                pizza = waiter.GetPizza();
+                Console.WriteLine("\n--- Margarita Pizza ---");
+                pizza.Info();
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Decorator
+{
+    public interface IChristmasTree
+    {
+        string Decorate();
+    }
+    public class ChristmasTree : IChristmasTree
+    {
+        public string Decorate()
+        {
+            return "Base Tree";
+        }
+    }
+    public abstract class TreeDecorator : IChristmasTree
+    {
+        protected IChristmasTree tree;
+
+        public TreeDecorator(IChristmasTree tree)
+        {
+            this.tree = tree;
+        }
+
+        public virtual string Decorate()
+        {
+            return tree.Decorate();
+        }
+    }
+    public class OrnamentsDecorator : TreeDecorator
+    {
+        private string ornaments = " + Balls and Tinsel";
+        public OrnamentsDecorator(IChristmasTree tree) : base(tree) { }
+        public override string Decorate()
+        {
+            return tree.Decorate() + ornaments;
+        }
+    }
+    public class GarlandsDecorator : TreeDecorator
+    {
+        public GarlandsDecorator(IChristmasTree tree) : base(tree) { }
+
+        public override string Decorate()
+        {
+            return tree.Decorate() + LightUp();
+        }
+        private string LightUp()
+        {
+            return " + Shining Garlands";
+        }
+    }
+    public class DecoratorExample
+    {
+        public static void Main(string[] args)
+        {
+            IChristmasTree simpleTree = new ChristmasTree();
+            Console.WriteLine($"Simple Tree: {simpleTree.Decorate()}");
+
+            IChristmasTree ornamentsOnly = new OrnamentsDecorator(new ChristmasTree());
+            Console.WriteLine($"Ornaments Only: {ornamentsOnly.Decorate()}");
+
+            IChristmasTree garlandsOnly = new GarlandsDecorator(new ChristmasTree());
+            Console.WriteLine($"Garlands Only: {garlandsOnly.Decorate()}");
+
+            IChristmasTree fullyDecoratedTree = new OrnamentsDecorator(
+                new GarlandsDecorator(new ChristmasTree())
+            );
+            Console.WriteLine($"Fully Decorated: {fullyDecoratedTree.Decorate()}");
+
+            Console.ReadKey();
+        }
+    }
+}
